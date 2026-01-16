6d5e5db696125dd377fa1845d37efa2935c043b2
Patyi Szabolcs <patyi.szabolcs@diak.szbi-pg.hu>
2026-01-16 08:36:41 +0100

    .

--- features/steps/step_definitions.py
+++ features/steps/step_definitions.py
@@ -2,9 +2,17 @@ from behave import given, when, then
 from src.is_it_friday import is_it_friday
 
 
-@given('today is "{day}"')
-def step_given_today_is(context, day):
-    pass
+@given('today is Sunday')
+def step_given_today_is(context):
+    context.today = "Sunday"
+
+@given('today is Friday')
+def step_given_today_is(context):
+    context.today = "Friday"
+
+@given('today is Monday')
+def step_given_today_is(context):
+    context.today = "Monday"
 
 
 @when("I ask whether it's Friday yet")
@@ -16,3 +24,4 @@ def step_when_i_ask_whether_its_friday_yet(context):
 def step_then_i_should_be_told(context, expected_answer):
     assert context.actual_answer == expected_answer, \
         f"Expected '{expected_answer}', but got '{context.actual_answer}'"
+
